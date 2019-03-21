8ed29a05e6047aa2f0ded7673763a49a1a214f13
daniilvs <dango41rus@yandex.ru>
2019-03-22 00:31:49 +0400

    1) Добавил оставшиеся функции 2) Добавил тесты 3) Добавил дополнительные методы для проверки

--- src/mybitset/BitSet.kt
+++ src/mybitset/BitSet.kt
@@ -4,7 +4,11 @@ package mybitset
 
 import java.lang.IllegalArgumentException
 
-class BitSet(private val list:MutableList<Int>, private var length: Int) {
+
+
+// data structure
+class BitSet(private var list:MutableList<Int>, private var length: Int) {
+
     init {
         if ((length < 0) || (length != list.size))
             throw NegativeArraySizeException("Wrong length (length < 0 or length doesn't equal to the actual size of bitset")
@@ -16,8 +20,7 @@ class BitSet(private val list:MutableList<Int>, private var length: Int) {
     fun and(second: BitSet): BitSet{
         equals(second)
         for (i in 0..(length - 1)) {
-            if ((list[i] == second.list[i] && list[i] == 1) ||
-                (list[i] == second.list[i] && list[i] == 0)) {
+            if (list[i] == second.list[i] && list[i] == 1) {
                 continue
             }
             else
@@ -26,7 +29,6 @@ class BitSet(private val list:MutableList<Int>, private var length: Int) {
         unZero()
         return this
     }
-
 
     /**   or logical function   */
     fun or(second: BitSet): BitSet {
@@ -42,28 +44,37 @@ class BitSet(private val list:MutableList<Int>, private var length: Int) {
         return this
     }
 
-    /**   add logical function   */
-    fun add(second: BitSet): BitSet = TODO() // Узнать, что именно подразумевается под "дополнением"
-               // method invocation
+    /**   not logical function   */
+    fun not(): BitSet {
+        val sup = mutableListOf<Int>()
+        for (element in this.list) {
+            if (element == 0)
+                sup.add(1)
+            else
+                sup.add(0)
+        }
+        sup.reversed()
+        this.list = sup
+        unZero()
+        return this
+    }
 
     /**   find element   */
     fun find(index: Int, element: Int): Boolean {
-        correctInput(element)
-        correctIndex(index)
+        correctAll(element, index)
         return list[index] == element
     }
 
     /**   put element/elements   */
-    // for one element
+    /* for one element */
     fun put(index: Int, element: Int): BitSet {
-        correctInput(element)
-        correctIndex(index)
+        correctAll(element, index)
         list.add(index, element)
         length++
         return this
     }
 
-    // for array
+    /* for array */
     fun put(index: Int, elements: List<Int>): BitSet {
         correctInput(elements)
         correctIndex(index)
@@ -73,7 +84,7 @@ class BitSet(private val list:MutableList<Int>, private var length: Int) {
     }
 
     /**   delete element/elements    */
-    // for one element
+    /* for one element */
     fun delete(index: Int): BitSet {
         correctIndex(index)
         list.removeAt(index)
@@ -81,7 +92,14 @@ class BitSet(private val list:MutableList<Int>, private var length: Int) {
         return this
     }
 
-    fun delete(index: Int, elements: List<Int>): Unit = TODO() // Что представляет из себя заданный массив элементов, который требуется удалить?
+    /* for array */
+    fun delete(index: Int, elements: List<Int>): BitSet {
+        correctIndex(index)
+        for (element in elements) {
+            delete(index)
+        }
+        return this
+    }
 
     /**   equate bitsets (add insignificant zero in the beginning)   */
     private fun equals(second: BitSet) {
@@ -123,26 +141,47 @@ class BitSet(private val list:MutableList<Int>, private var length: Int) {
 
     /**   Is index correct? ;)   */
     private fun correctIndex(index: Int) {
-        if (index !in 0..(length-1))
+        if (index !in 0..(length - 1))
             throw IllegalArgumentException("Wrong index")
     }
 
     /**   Is element correct?   */
-    // for one element
+    /* for one element */
     private fun correctInput(element: Int) {
         if (!(listOf(0, 1).contains(element)))
             throw IllegalArgumentException("Wrong input data")
     }
 
-    // for array
+    /* for array */
     private fun correctInput(elements: List<Int>) {
         if (!(listOf(0, 1).containsAll(elements)))
             throw IllegalArgumentException("Wrong input data")
     }
 
+    private fun correctAll(element: Int, index: Int) {
+        correctInput(element)
+        correctIndex(index)
+    }
 
     override fun toString(): String {
         return "BitSet is $list, size is $length"
     }
 
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as BitSet
+
+        if (list != other.list) return false
+        if (length != other.length) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = list.hashCode()
+        result = 31 * result + length
+        return result
+    }
 }
--- src/test/Test.kt
+++ src/test/Test.kt
@@ -4,41 +4,57 @@ import mybitset.BitSet
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
-//   В тестах and и or выдает не самую понятную ошибку:
-//   org.opentest4j.AssertionFailedError: expected: mybitset.BitSet@3d99d22e<BitSet is [1, 0], size is 2> but was: mybitset.BitSet@49fc609f<BitSet is [1, 0], size is 2>
-//     Expected :BitSet is [1, 0], size is 2
-//     Actual   :BitSet is [1, 0], size is 2
-
 class Tests {
-    private val testBitSet1 =
-        BitSet(mutableListOf(1,0,1,0,0,0,1,0,1,1), 10)
-
 
     @Test
     fun and() {
-        assertEquals(BitSet(mutableListOf(1,0), 2), testBitSet1
-            .and(BitSet(mutableListOf(0,1,1,0,0,0,1,0), 8)))
-        assertEquals(BitSet(mutableListOf(1,0,1,1), 4), testBitSet1
-            .and(BitSet(mutableListOf(0,1,1,0,1,0,1,1), 8)))
-        assertThrows(java.lang.NegativeArraySizeException::class.java) { testBitSet1
-            .and(BitSet(mutableListOf(0,1,1,0,0,0,1,0), 0))}
-        assertThrows(java.lang.NegativeArraySizeException::class.java) { testBitSet1
-            .and(BitSet(mutableListOf(0,1,1,0,0,0,1,0), -6))}
-        assertThrows(java.lang.IllegalArgumentException::class.java) { testBitSet1
-            .and(BitSet(mutableListOf(0,1,1,0,0,2,1,0), 8))}
+        var testBitSet =
+            BitSet(mutableListOf(1,0,1,0,0,0,1,0,1,1), 10)
+        assertEquals(BitSet(mutableListOf(1,0), 2),
+            testBitSet.and(BitSet(mutableListOf(0,1,1,0,0,0,1,0), 8)))
+        testBitSet = BitSet(mutableListOf(1,0,1,0,0,0,1,0,1,1), 10)
+        assertEquals(BitSet(mutableListOf(1,0,1,1), 4),
+            testBitSet.and(BitSet(mutableListOf(0,1,1,0,1,0,1,1), 8)))
+        testBitSet = BitSet(mutableListOf(1,0,1,0,0,0,1,0,1,1), 10)
+        assertThrows(java.lang.NegativeArraySizeException::class.java) {
+            testBitSet.and(BitSet(mutableListOf(0,1,1,0,0,0,1,0), 0)) }
+        testBitSet = BitSet(mutableListOf(1,0,1,0,0,0,1,0,1,1), 10)
+        assertThrows(java.lang.NegativeArraySizeException::class.java) {
+            testBitSet.and(BitSet(mutableListOf(0,1,1,0,0,0,1,0), -6)) }
+        testBitSet = BitSet(mutableListOf(1,0,1,0,0,0,1,0,1,1), 10)
+        assertThrows(java.lang.IllegalArgumentException::class.java) {
+            testBitSet.and(BitSet(mutableListOf(0,1,1,0,0,2,1,0), 8)) }
     }
 
     @Test
     fun or() {
-        assertEquals(BitSet(mutableListOf(1,0,1,0,0,0,1,1,1,1), 10), testBitSet1
-            .or(BitSet(mutableListOf(1,0,0), 3)))
+        var testBitSet = BitSet(mutableListOf(1,0,1,0,0,0,1,0,1,1), 10)
+        assertEquals(BitSet(mutableListOf(1,0,1,0,0,0,1,1,1,1), 10),
+            testBitSet.or(BitSet(mutableListOf(1,0,0), 3)))
+        testBitSet = BitSet(mutableListOf(1,0,1,0,0,0,1,0,1,1), 10)
+        assertThrows(java.lang.NegativeArraySizeException::class.java) {
+            testBitSet.and(BitSet(mutableListOf(0,1,1,0,0,0,1,0), 0)) }
+        testBitSet = BitSet(mutableListOf(1,0,1,0,0,0,1,0,1,1), 10)
+        assertThrows(java.lang.NegativeArraySizeException::class.java) {
+            testBitSet.and(BitSet(mutableListOf(0,1,1,0,0,0,1,0), -6)) }
     }
 
     @Test
     fun find() {
-        assertFalse(testBitSet1.find(3, 1))
-        assertTrue(testBitSet1.find(0, 1))
-        assertThrows(java.lang.IllegalArgumentException::class.java) { testBitSet1.find(-1, 6)}
+        val testBitSet =
+            BitSet(mutableListOf(1,0,1,0,0,0,1,0,1,1), 10)
+        assertFalse(testBitSet.find(3, 1))
+        assertTrue(testBitSet.find(0, 1))
+        assertThrows(java.lang.IllegalArgumentException::class.java) {
+            testBitSet.find(-1, 6) }
     }
 
+    @Test
+    fun not() {
+        var testBitSet = BitSet(mutableListOf(1,0,1,0,0,0,1,0,1,1), 10)
+        assertEquals(BitSet(mutableListOf(1,0,1,1,1,0,1,0,0), 9), testBitSet.not())
+        testBitSet = BitSet(mutableListOf(0,1,0,1,1), 5)
+        assertEquals(BitSet(mutableListOf(1,0,1,0,0), 5), testBitSet.not())
+
+    }
 }
